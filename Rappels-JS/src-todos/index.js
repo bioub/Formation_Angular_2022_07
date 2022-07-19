69051c7b4a959722e646830090552e6e7bfac8e1
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-07-19 15:20:27 +0200

    Correction DOM

--- Rappels-JS/src-todos/index.js
+++ Rappels-JS/src-todos/index.js
@@ -9,6 +9,9 @@ const inputEl = document.querySelector('.todo-form-input');
 /** @type {HTMLDivElement} */
 const listEl = document.querySelector('.todos-list');
 
+/** @type {HTMLInputElement} */
+const toggleEl = document.querySelector('.todo-form-toggle');
+
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -21,8 +24,29 @@ formEl.addEventListener('submit', (event) => {
   listEl.append(itemEl);
 });
 
+
+listEl.addEventListener('click', (event) => {
+  // console.log('event.target', event.target);
+  // console.log('event.currentTarget', event.currentTarget);
+  // event.currentTarget l'élément sur lequel on a appelé addEventListener
+  // event.target l'élément le plus profond de l'arbre sur lequel
+
+  if (event.target.classList.contains('todo-item-button-delete')) {
+    event.target.closest('.todo-item').remove();
+  }
+});
+
 // Exercice 3
 // Sélectionner la checkbox
 // <input type="checkbox" class="todo-form-toggle" />
 // au clic de celle-ci, cocher ou décocher toutes les
 // checkbox dans todo-item (créée à l'exercice 1)
+
+toggleEl.addEventListener('click', () => {
+  /** @type {NodeListOf<HTMLInputElement>} */
+  const checkboxEls = listEl.querySelectorAll('.todo-item-completed');
+
+  for (const checkboxEl of checkboxEls) {
+    checkboxEl.checked = toggleEl.checked;
+  }
+});
--- Rappels-JS/src-todos/todos.js
+++ Rappels-JS/src-todos/todos.js
@@ -24,12 +24,60 @@ export function createTodoItem(todo) {
   divEl.className = "todo-item";
   divEl.dataset.todoId = String(todo.id);
 
-  const spanEl = document.createElement('span');
-  spanEl.className = "todo-item-title";
-  spanEl.innerText = todo.title;
+  const checkboxEl = createTodoItemCompleted(todo)
+  divEl.append(checkboxEl);
 
+  const spanEl = createTodoItemTitle(todo);
   divEl.append(spanEl);
+
+  const buttonEl = createTodoItemButtonDelete(todo);
+  divEl.append(buttonEl);
 
   return divEl;
 }
+
+/**
+ * @param {object} todo
+ * @param {number} todo.id
+ * @param {string} todo.title
+ * @param {boolean} todo.completed
+ * @returns {HTMLInputElement}
+ */
+function createTodoItemCompleted(todo) {
+  const inputEl = document.createElement('input');
+  inputEl.type = 'checkbox';
+  inputEl.className = "todo-item-completed";
+  inputEl.checked = todo.completed;
+  return inputEl;
+}
+
+/**
+ * @param {object} todo
+ * @param {number} todo.id
+ * @param {string} todo.title
+ * @param {boolean} todo.completed
+ * @returns {HTMLSpanElement}
+ */
+function createTodoItemTitle(todo) {
+  const spanEl = document.createElement('span');
+  spanEl.className = "todo-item-title";
+  spanEl.innerText = todo.title;
+  return spanEl;
+}
+
+/**
+ * @param {object} todo
+ * @param {number} todo.id
+ * @param {string} todo.title
+ * @param {boolean} todo.completed
+ * @returns {HTMLButtonElement}
+ */
+ function createTodoItemButtonDelete(todo) {
+  const buttonEl = document.createElement('button');
+  buttonEl.className = "todo-item-button-delete";
+  buttonEl.innerText = '-';
+
+  return buttonEl;
+}
+
 
