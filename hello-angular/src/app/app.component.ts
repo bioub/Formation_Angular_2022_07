370fbb261e409cf712ea6fcaf9de80ff5294967a
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-07-25 17:38:35 +0200

    Fin Angular

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -8,6 +8,11 @@ export class AppComponent {
 
   prenom = 'Toto';
 
+  // pour l'horloge
+  format = 'HH:mm:ss';
+  delay = 2000;
+  show = true;
+
   handleChange(val: any) {
     this.prenom = val;
   }
--- hello-angular/src/app/clock/clock.component.ts
+++ hello-angular/src/app/clock/clock.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.css']
+  styleUrls: ['./clock.component.css'],
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() format = 'HH:mm:ss';
+  @Input() delay = 1000;
 
   now = new Date();
 
+  interval: any;
+
   constructor() {
-    setInterval(() => {
-      this.now = new Date();
-      // detectChanges();
-      // grace à Zone.js on peut intercepter les callbacks asynchrones
-    }, 1000);
+    console.log('constructor', this.delay);
   }
 
   ngOnInit(): void {
+    console.log('ngOnInit', this.delay);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['delay']?.previousValue !== changes['delay']?.currentValue) {
+      clearInterval(this.interval);
+      this.interval = setInterval(() => {
+        this.now = new Date();
+        // detectChanges();
+        // grace à Zone.js on peut intercepter les callbacks asynchrones
+      }, this.delay);
+    }
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
